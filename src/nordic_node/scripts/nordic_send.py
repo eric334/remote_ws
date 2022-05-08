7ce8e3402972339aafe3e51ef7fb2ee9058e7eb2
Eric Buckland <eric334@ucsb.edu>
2022-05-08 12:18:55 -0700

    remove unnecesary deprecated code

--- src/nordic_node/scripts/nordic_send.py
+++ src/nordic_node/scripts/nordic_send.py
@@ -9,31 +9,42 @@ import dynamic_reconfigure.client
 class Node:
 
     def __init__(self):
-
         twist_topic = rospy.get_param("~twist_topic")
         empty_topic = rospy.get_param("~empty_topic")
+        reply_topic = rospy.get_param("~reply_topic")
+
+        self.enable = rospy.get_param("~enable")
+
+        self.send_deploy
 
         dev = rospy.get_param("~dev", "/dev/ttyACM0")
         baud = int(rospy.get_param("~baud", "115200"))
 
         rospy.loginfo("Nordic_send - opening serial : " + dev)
         # TODO COPY SPECIFICS
-        self.serial = Serial(dev, timeout=1, baudrate=baud)
+
+        if self.enable:
+            self.serial = Serial(dev, timeout=1, baudrate=baud)
         # TODO may need to close open
+
+        # only need a callback on reply
 
         self.sub_camera = rospy.Subscriber(twist_topic, Twist, self.callback)
         rospy.loginfo("Nordic_send - subscribed to topic : " + twist_topic)
         self.sub_hector = rospy.Subscriber(empty_topic, Empty, self.callback)
         rospy.loginfo("Nordic_send - subscribed to topic : " + empty_topic)
+        self.sub_reply = rospy.Subscriber(empty_topic, Empty, self.callback)
 
     def run(self):
         rospy.spin()
 
     def callback_twist(self, data):
-        self.write_serial(data)
+        if self.enable:
+            self.write_serial(data)
 
     def write_serial(self, data):
-        self.serial.write(data)
+        if self.enable:
+            self.serial.write(data)
         
 if __name__ == '__main__':
     rospy.init_node('nordic_send', anonymous=True)
